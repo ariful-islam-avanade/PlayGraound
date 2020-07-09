16f2d6f44db208e8ae90fc3a041fb76d384a0c9a
Ariful Islam <ariful.islam@avanade.com>
2020-07-09 14:50:08 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayGraound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
